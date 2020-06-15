80dda520e0a16d0c9d788a22a119a792531a4f41
jorgedsadasdasdads <66454983+jorgedsadasdasdads@users.noreply.github.com>
2020-06-15 14:15:19 -0300

    bot

--- index.js
+++ index.js
@@ -1,106 +1,17 @@
 if (process.version.slice(1).split('.')[0] < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.')
 
-require('dotenv').config()
 
 const Discord = require('discord.js')
-const { readdirSync } = require('fs')
-const Enmap = require('enmap')
 const client = new Discord.Client()
-
-client.commands = new Enmap()
 client.startTime = Date.now()
 
-const cmdFiles = readdirSync('./commands/')
 console.log('log', `Carregando o total de ${cmdFiles.length} comandos.`)
 
-cmdFiles.forEach(f => {
-  try {
-    const props = require(`./commands/${f}`)
-    if (f.split('.').slice(-1)[0] !== 'js') return
 
     console.log('log', `Carregando comando: ${props.help.name}`)
 
     if (props.init) props.init(client)
 
-    client.commands.set(props.help.name, props)
-    if (props.help.aliases) {
-      props.alias = true
-      props.help.aliases.forEach(alias => client.commands.set(alias, props))
-    }
-  } catch (e) {
-    console.log(`Impossivel executar comando ${f}: ${e}`)
-  }
-})
-
-const evtFiles = readdirSync('./events/')
-console.log('log', `Carregando o total de ${evtFiles.length} eventos`)
-evtFiles.forEach(f => {
-  const eventName = f.split('.')[0]
-  const event = require(`./events/${f}`)
-
-  client.on(eventName, event.bind(null, client))
-})
-
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Expulso com sucesso, usuário: ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('Eu não posso expulsar este membro!');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("O usuário não está nesse servidor!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("Você não mencionou nenhum usuário!");
-    }
-  }
-});
-
-client.on('message', message => {
-  // If the message is "p"
-  if (message.content === '!convite') {
-    // Send "pong" to the same channel
-    message.channel.send('Me convide para seu servidor: https://discordapp.com/oauth2/authorize?client_id=717813113138708500&scope=bot&permissions=2146958847');
-  }
-});
-
-client.on('message', message => {
-  // If the message is "p"
-  if (message.content === '!suporte') {
-    // Send "pong" to the same channel
-    message.channel.send('Entre no meu servidor de suporte: https://discord.gg/spMtCft');
-  }
-});
 
 
 
